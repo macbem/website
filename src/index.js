1e2cea7d72a6cf2c9146f5bf2244e747423d07f0
Chi Kei Chan <chikeichan@gmail.com>
2018-11-03 10:45:38 +0100

    Add Google Analytics

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
 import './index.scss';
 import App from './App';
+
+if (process.env.NODE_ENV === 'production') {
+  ReactGA.initialize('UA-128182339-2');
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
